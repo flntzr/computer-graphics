5143bafe5d4832cb1876052d343158126824b7ff
Franz <fflintzer@gmail.com>
2018-12-04 12:49:19 +0100

    normalize all vectors and remove any colors from the phong-polygon for debugging reasons

--- src/res/shaders/phong_v.glsl
+++ src/res/shaders/phong_v.glsl
@@ -11,7 +11,7 @@ uniform mat4 projection_matrix;
 
 float n = 1; // hardness
 float iL = 2.0; // light intensity
-vec3 l = vec3(3, 0, 0); // light source
+vec3 l = normalize(vec3(3, 0, 2)); // light source
 float iA = .3;
 float kA = .4;
 float kS = .2;
@@ -19,11 +19,11 @@ float kD = .4;
 
 void main() {
 	mat4 inverseMatrix = inverse(transpose(matrix));
-//	float alpha = dot(vec4(-1, 1, 1, 0), inverseMatrix * vec4(normal_vectors, 0)); // angle between light vector and camera vector
 	vec3 normal_vector = vec3(inverseMatrix) * normal_vectors;
-	vec3 r = 2 * (l * normal_vectors) * normal_vectors - l;
-	vec3 v = vec3(-1, 1, 1); // direction to camera
+	normal_vector = normalize(normal_vector);
+	vec3 r = 2 * dot(l, normal_vectors) * normal_vectors - l;
+	vec3 v = normalize(vec3(-1, 1, 1)); // direction to camera
 	float i = iA * kA + iL * (dot(l, normal_vector)* kD + pow(dot(r, v), n) * kS);
-	triangleColor = colors * i;
+	triangleColor = vec3(i, i, i);
 	gl_Position = projection_matrix * matrix * vec4(positions, 1.0);
 }
